47452de0b0625e65f31e333460c59ffc0457406c
alassane seck <aseck9719@gmail.com>
2025-10-06 00:24:52 +0200

    test modif

--- src/main/java/alassane/seck/gddapi/filter/JwtFilter.java
+++ src/main/java/alassane/seck/gddapi/filter/JwtFilter.java
@@ -44,7 +44,6 @@ public class JwtFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
-
         filterChain.doFilter(request, response);
     }
 }
