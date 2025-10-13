7104a436d27985141f2aa3c820222f8ed21547f6
alassane seck <aseck9719@gmail.com>
2025-10-13 02:05:32 +0200

    update file

--- src/main/java/alassane/seck/gddapi/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package alassane.seck.gddapi.repository;
-
-import alassane.seck.gddapi.entities.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
